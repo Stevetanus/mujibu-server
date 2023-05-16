856b0db5d55462088d31d0f6c1fea2b8c753c24a
Chian <chian0322@gmail.com>
2023-05-16 23:18:28 +0800

    fix: add BASE_URL option for Render url

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -13,7 +13,7 @@ const swaggerDef = {
   },
   servers: [
     {
-      url: `http://localhost:${config.port}/v1`,
+      url: `${process.env.BASE_URL || `http://localhost:${config.port}`}/v1`,
     },
   ],
 };
