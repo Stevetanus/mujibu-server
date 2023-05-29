16468706c76ec50b6041633460e0cc056e035a09
Chian <chian0322@gmail.com>
2023-05-29 22:28:36 +0800

    fix: joi valid updateUser required

--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -32,30 +32,30 @@ const updateUser = {
   }),
   body: Joi.object()
     .keys({
-      teamId: Joi.array().items(Joi.string().custom(objectId)),
-      avatar: Joi.string(),
-      name: Joi.string(),
-      nickname: Joi.string(),
-      birthDate: Joi.date(),
-      gender: Joi.number().valid(0, 1, 2).allow(null).optional(), // 'male', 'female', 'other'
-      phone: Joi.string(),
-      subscribeNewsletter: Joi.boolean(),
+      // teamId: Joi.array().items(Joi.string().custom(objectId)).optional(),
+      avatar: Joi.string().allow('').optional(),
+      name: Joi.string().required(),
+      nickname: Joi.string().required(),
+      birthDate: Joi.date().required(),
+      gender: Joi.number().valid(0, 1, 2).required(), // 'male', 'female', 'other'
+      phone: Joi.string().required(),
+      subscribeNewsletter: Joi.boolean().allow('').optional(),
       category: Joi.array()
         .items(Joi.string().valid(...['藝術', '設計', '電影', '音樂', '科技', '出版']))
-        .allow(null)
+        .allow('')
         .optional(),
-      contactName: Joi.string(),
-      commentName: Joi.string(),
-      contactPhone: Joi.string(),
-      countryCode: Joi.string(),
-      postalCode: Joi.string(),
-      city: Joi.string(),
-      district: Joi.string(),
-      address: Joi.string(),
-      role: Joi.string().valid('user', 'admin'), // 根據你的角色列表進行更改
-      notifications: Joi.array().items(Joi.string().custom(objectId)),
-      collects: Joi.array().items(Joi.string().custom(objectId)),
-      isEmailVerified: Joi.boolean(),
+      contactName: Joi.string().allow('').optional(),
+      commentName: Joi.string().allow('').optional(),
+      contactPhone: Joi.string().allow('').optional(),
+      countryCode: Joi.string().allow('').optional(),
+      postalCode: Joi.string().allow('').optional(),
+      city: Joi.string().allow('').optional(),
+      district: Joi.string().allow('').optional(),
+      address: Joi.string().allow('').optional(),
+      // role: Joi.string().valid('user', 'admin'), // 根據你的角色列表進行更改
+      // notifications: Joi.array().items(Joi.string().custom(objectId)),
+      // collects: Joi.array().items(Joi.string().custom(objectId)),
+      // isEmailVerified: Joi.boolean(),
     })
     .min(1),
 };
