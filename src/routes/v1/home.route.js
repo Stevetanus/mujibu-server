15011d7286b1d86e935c160ba6daed9f16b3c1a7
Chian <chian0322@gmail.com>
2023-06-05 23:28:05 +0800

    docs: upadte home swagger ui

--- src/routes/v1/home.route.js
+++ src/routes/v1/home.route.js
@@ -15,15 +15,131 @@ module.exports = router;
  * @swagger
  * tags:
  *   name: Home
- *   description: Home
+ *   description: Home management and retrieval
  */
 
 /**
  * @swagger
  * /home/hot:
  *   get:
- *     summary: Get hot type: '0' & sortBy: 'backers:desc'
- *     description: get from project.
+ *     summary: Get type 0 & sortBy backers desc.
+ *     description: get projects hot.
+ *     tags: [Home]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Project'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /home/carousel:
+ *   get:
+ *     summary: Get carousel true.
+ *     description: get projects carousel.
+ *     tags: [Home]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Project'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /home/picks:
+ *   get:
+ *     summary: Get type 1 & sortBy score desc.
+ *     description: get projects picks.
+ *     tags: [Home]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Project'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /home/success:
+ *   get:
+ *     summary: Get type 2 & sortBy currentAmountPercentage desc.
+ *     description: get projects success.
+ *     tags: [Home]
+ *     security:
+ *       - BearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Project'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /home/new:
+ *   get:
+ *     summary: Get type 0 & sortBy startTime desc.
+ *     description: get projects new.
  *     tags: [Home]
  *     security:
  *       - BearerAuth: []
